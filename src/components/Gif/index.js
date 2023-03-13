65f2b6e08295bac966f4bf3c6cc5814d6f959d58
Jordi Tomas <j.tomas@telfy.com>
2023-03-13 10:11:19 +0100

    Grid design

--- src/components/Gif/index.js
+++ src/components/Gif/index.js
@@ -5,11 +5,11 @@ import { Link } from 'wouter'
 
 export default function Gif ({title, id, url}) {
     return (
-        <div>
+        <>
             <Link to={`/gif/${id}`} className="Gif">
                 <h4>{title}</h4>
                 <img alt={title} src={url} />
             </Link>
-        </div>
+        </>
     )
 }
